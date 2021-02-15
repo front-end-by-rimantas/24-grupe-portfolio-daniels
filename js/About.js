b5da80fd3cf7f2301dd1132b567ef1c2f0f5c9d0
Ernestas <ernesto.kristy@gmail.com>
2021-02-15 18:46:40 +0200

    innitial comments commit

---
+++ js/About.js
@@ -0,0 +1,27 @@
+// IMPORTS
+
+// header
+// hero
+// about me
+// services
+// portfolio
+// testimonials
+// testimonial numbers
+// blog
+// contact us
+// footer
+
+// ------------------------------------------------------------------
+
+// EXECUTION
+
+// header
+// hero
+// about me
+// services
+// portfolio
+// testimonials
+// testimonial numbers
+// blog
+// contact us
+// footer
