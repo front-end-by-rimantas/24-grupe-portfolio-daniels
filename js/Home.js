14c063defe93bfcec39edbfbbf61b059e1a95610
mildapaulavi <76951460+mildapaulavi@users.noreply.github.com>
2021-02-23 10:04:40 +0200

    Socials

--- js/Home.js
+++ js/Home.js
@@ -1,6 +1,11 @@
 import { navSlide } from './components/Nav.js';
 import { navCheck } from './components/Nav.js';
 
+import { renderSocials } from './components/renderSocials.js';
+import { socialsData } from './data/socialsData.js'
+
+
+renderSocials('.socials', socialsData);
 
 navSlide();
 navCheck();
